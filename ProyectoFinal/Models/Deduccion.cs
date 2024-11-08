a4141d344efd78b32cc3fc5c0fc68adbc96937b3
priscila0621 <pjulieths@uamv.edu.ni>
2024-11-07 19:41:42 -0600

    Se agregó la clase Deduccion

---
+++ ProyectoFinal/Models/Deduccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Models
+{
+    public struct Deduccion
+    {
+        public int EmpleadoID { get; set; }
+        public string TipoDeduccion { get; set; }
+        public double Monto { get; set; }
+    }
+}
